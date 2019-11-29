a838334a3fec12f07e5bad3514461033cef5a37f
Damodar Lohani <dlohani48@gmail.com>
2019-11-29 20:43:42 +0545

    new additions

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
+import 'dart:convert';
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'package:table_calendar/table_calendar.dart';
 
 void main() => runApp(MyApp());
@@ -23,12 +25,26 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   CalendarController _controller;
-
   @override
   void initState() {
-    // TODO: implement initState
     super.initState();
     _controller = CalendarController();
+  }
+  
+  Map<String,dynamic> encodeMap(Map<DateTime,dynamic> map) {
+    Map<String,dynamic> newMap = {};
+    map.forEach((key,value) {
+      newMap[key.toString()] = map[key];
+    });
+    return newMap;
+  }
+
+  Map<DateTime,dynamic> decodeMap(Map<String,dynamic> map) {
+    Map<DateTime,dynamic> newMap = {};
+    map.forEach((key,value) {
+      newMap[DateTime.parse(key)] = map[key];
+    });
+    return newMap;
   }
 
   @override
@@ -44,6 +60,7 @@ class _HomePageState extends State<HomePage> {
             TableCalendar(
               initialCalendarFormat: CalendarFormat.week,
               calendarStyle: CalendarStyle(
+                canEventMarkersOverflow: true,
                   todayColor: Colors.orange,
                   selectedColor: Theme.of(context).primaryColor,
                   todayStyle: TextStyle(
@@ -61,7 +78,7 @@ class _HomePageState extends State<HomePage> {
               ),
               startingDayOfWeek: StartingDayOfWeek.monday,
               onDaySelected: (date, events) {
-                print(date.toIso8601String());
+
               },
               builders: CalendarBuilders(
                 selectedDayBuilder: (context, date, events) => Container(
